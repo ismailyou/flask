270678da3b1138a9062ece7946b923c056115cb6
ismail jadid <ismail.jadid.tdi@gmail.com>
2022-07-19 15:03:01 +0100

    database credentials

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///database.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URLL","sqlite:///database.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "soc"
 api = Api(app)
